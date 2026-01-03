8d133d41080c224a8e57e348daec6022e214bbf5
Hugh Bellamy <hughbellars@gmail.com>
2026-01-03 13:39:21 +0000

    Fix build

--- Explorer/OrgPlusChartDumper/Program.cs
+++ Explorer/OrgPlusChartDumper/Program.cs
@@ -8,7 +8,7 @@ var chart = new OrganizationChart(stream);
 
 foreach (var record in chart.Records)
 {
-    Console.WriteLine($"{record.id}: {GetRecordDetails((OrganizationChartRecordId)record.id, record.data)}");
+    Console.WriteLine($"{record.Id}: {GetRecordDetails((OrganizationChartRecordId)record.Id, record.Bytes)}");
 }
 
 static string GetRecordDetails(OrganizationChartRecordId id, byte[] data)
@@ -17,25 +17,25 @@ static string GetRecordDetails(OrganizationChartRecordId id, byte[] data)
     using var reader = new BinaryReader(stream);
     object obj = id switch
     {
-        OrganizationChartRecordId.BoxTextPartCount => new BoxTextPartCountRecord(reader),
-        OrganizationChartRecordId.BoxTextPartIndex => new BoxTextPartIndexRecord(reader),
-        OrganizationChartRecordId.BoxTextPartType => new BoxTextPartTypeRecord(reader),
-        OrganizationChartRecordId.BodyInfo => new BodyInfoRecord(reader),
-        OrganizationChartRecordId.FontName => new FontNameRecord(reader),
-        OrganizationChartRecordId.TitlePartIndex => new TitlePartIndexRecord(reader),
-        OrganizationChartRecordId.Level => new LevelRecord(reader),
-        OrganizationChartRecordId.BoxIndex => new BoxIndexRecord(reader),
-        OrganizationChartRecordId.Border => new BorderRecord(reader),
-        OrganizationChartRecordId.Connector => new ConnectorRecord(reader),
-        OrganizationChartRecordId.Line => new LineRecord(reader),
-        OrganizationChartRecordId.Text => new TextRecord(reader),
-        OrganizationChartRecordId.Shape => new ShapeRecord(reader),
-        OrganizationChartRecordId.ShapeColor => new ShapeColorRecord(reader),
-        OrganizationChartRecordId.Selected => new SelectedRecord(reader),
-        OrganizationChartRecordId.Shadow => new ShadowRecord(reader),
-        OrganizationChartRecordId.FontIndex => new FontIndexRecord(reader),
-        OrganizationChartRecordId.Footer => new FooterRecord(reader),
-        OrganizationChartRecordId.GlobalInfo => new GlobalInfoRecord(reader),
+        OrganizationChartRecordId.BoxTextPartCount => new BoxTextPartCountRecord(id, reader),
+        OrganizationChartRecordId.BoxTextPartIndex => new BoxTextPartIndexRecord(id, reader),
+        OrganizationChartRecordId.BoxTextPartType => new BoxTextPartTypeRecord(id, reader),
+        OrganizationChartRecordId.BodyInfo => new BodyInfoRecord(id, reader),
+        OrganizationChartRecordId.FontName => new FontNameRecord(id, reader),
+        OrganizationChartRecordId.TitlePartIndex => new TitlePartIndexRecord(id, reader),
+        OrganizationChartRecordId.Level => new LevelRecord(id, reader),
+        OrganizationChartRecordId.BoxIndex => new BoxIndexRecord(id, reader),
+        OrganizationChartRecordId.Border => new BorderRecord(id, reader),
+        OrganizationChartRecordId.Connector => new ConnectorRecord(id, reader),
+        OrganizationChartRecordId.Line => new LineRecord(id, reader),
+        OrganizationChartRecordId.Text => new TextRecord(id, reader),
+        OrganizationChartRecordId.Shape => new ShapeRecord(id, reader),
+        OrganizationChartRecordId.ShapeColor => new ShapeColorRecord(id, reader),
+        OrganizationChartRecordId.Selected => new SelectedRecord(id, reader),
+        OrganizationChartRecordId.Shadow => new ShadowRecord(id, reader),
+        OrganizationChartRecordId.FontIndex => new FontIndexRecord(id, reader),
+        OrganizationChartRecordId.Footer => new FooterRecord(id, reader),
+        OrganizationChartRecordId.GlobalInfo => new GlobalInfoRecord(id, reader),
         _ => throw new NotSupportedException($"Record ID {id} is not supported."),
     };
     return obj.ToString()!;
